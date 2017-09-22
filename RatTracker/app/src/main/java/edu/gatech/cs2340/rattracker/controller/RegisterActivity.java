a4cb5938ef058b06b18e26d0caf551754c5716fa
Kevin Liao <kliao2016@gmail.com>
2017-09-22 03:51:14 -0400

    Finished login page and Firebase works

---
+++ RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/RegisterActivity.java
@@ -0,0 +1,42 @@
+package edu.gatech.cs2340.rattracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance();
+    }
+
+    /*
+        Method to create user account based on inputted info
+     */
+    private void createUserAccount(String email, String password) {
+        this.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //TODO 1: Create segue to application here
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //TODO 2: Create popup notifying sign in failure
+                        }
+                    }
+                });
+    }
+}
