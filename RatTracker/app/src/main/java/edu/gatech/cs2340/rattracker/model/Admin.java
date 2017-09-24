8eae845be8ab73891544e9c75a2379a687dbc926
Jackson Hall <jacksonhall@lawn-128-61-56-253.lawn.gatech.edu>
2017-09-24 17:16:27 -0400

    Initial commit of User and Admin classes with basic constructors, getters, and setters and method headers with for the more complex methods.

---
+++ RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/model/Admin.java
@@ -0,0 +1,19 @@
+public class Admin extends User {
+    public Admin(String username, String password, String email) {
+        super(username, password, email, false);
+    }
+    public boolean addUser(User user) {
+        //TODO implement
+        return false;
+    }
+    public boolean removeUser(User user) {
+        //TODO implement
+        return false;
+    }
+    public void unlock(User user) {
+        user.locked = false;
+    }
+    public void lock(User user) {
+        user.locked = true;
+    }
+}
---
+++ RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/model/User.java
@@ -0,0 +1,44 @@
+public class User {
+
+    private String username, password, email;
+    private Boolean locked;
+
+    public User(String username, String password, String email, boolean locked) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.locked = locked;
+    }
+
+    public User(String username, String password, String email) {
+        this(username,password,email, false);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public boolean getLocked() {
+        return this.locked;
+    }
+
+    public void setUsername(String newUsername) {
+        this.username = newUsername;
+    }
+
+    public void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
+}
