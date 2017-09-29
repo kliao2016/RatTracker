061ef9fe7b87f73e939d3997c68fa1e52f6f12db
Jackson Hall <jacksonhall@lawn-128-61-60-41.lawn.gatech.edu>
2017-09-29 14:11:33 -0400

    Added javadocs to model and modified constructor for admin.

--- RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/model/User.java
+++ RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/model/User.java
@@ -1,10 +1,22 @@
 package edu.gatech.cs2340.rattracker.model;
 
+/**
+ * @author Charles Hall
+ * @version 1.0
+ */
+
 public class User {
 
     private String username, password, email;
     private Boolean locked;
 
+    /**
+     *
+     * @param username
+     * @param password
+     * @param email
+     * @param locked if the user has been locked out by an admin
+     */
     public User(String username, String password, String email, boolean locked) {
         this.username = username;
         this.password = password;
@@ -12,36 +24,75 @@ public class User {
         this.locked = locked;
     }
 
+    /**
+     * Constructor with a default value of false for locked
+     *
+     * @param username
+     * @param password
+     * @param email
+     */
     public User(String username, String password, String email) {
         this(username,password,email, false);
     }
 
+    /**
+     *
+     * @return username of user
+     */
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     *
+     * @return user password
+     */
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     *
+      * @return user email
+     */
     public String getEmail() {
         return this.email;
     }
 
+    /**
+     *
+     * @return if the user has been locked
+     */
     public boolean getLocked() {
         return this.locked;
     }
 
+    /**
+     *
+     * @param lockedValue the value desired for locked
+     */
     public void setLocked(boolean lockedValue) { this.locked = lockedValue; }
 
+    /**
+     *
+     * @param newUsername the new username desired
+     */
     public void setUsername(String newUsername) {
         this.username = newUsername;
     }
 
+    /**
+     *
+     * @param newPassword the new password desired
+     */
     public void setPassword(String newPassword) {
         this.password = newPassword;
     }
 
+    /**
+     *
+     * @param newEmail the new email desired
+     */
     public void setEmail(String newEmail) {
         this.email = newEmail;
     }
