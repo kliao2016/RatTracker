9d6eb9ac9b729ff93180cbd58106f84e83bb413a
ClosestExaminer <kyle71698@gmail.com>
2017-10-10 15:15:40 -0400

    added report list content

--- RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/ReportListActivity.java
+++ RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/ReportListActivity.java
@@ -1,4 +1,19 @@
 package edu.gatech.cs2340.rattracker.controller;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 public class ReportListActivity {
+
+    public class ReportViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ReportViewHolder(View view) {
+            super(view);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
 }
