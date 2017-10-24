9e58f2bb6ae9d833ef52d54b35d1d1b2925f97cb
Jackson Hall <jacksonhall@lawn-128-61-49-228.lawn.gatech.edu>
2017-10-24 17:13:14 -0400

    Added start date and end date to the map fragment and set listeners on the the buttons to show calenders to select the start date and end date.

--- RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/RatMapActivity.java
+++ RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/RatMapActivity.java
@@ -1,7 +1,13 @@
 package edu.gatech.cs2340.rattracker.controller;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,15 +21,23 @@ import edu.gatech.cs2340.rattracker.R;
 public class RatMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static EditText dateText;
+    private static EditText timeText;
+    private Button startDateButton;
+    private Button endDateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rat_map);
+        startDateButton = findViewById(R.id.startDate);
+        endDateButton = findViewById(R.id.endDate);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        setClickListeners();
     }
 
 
@@ -46,5 +60,35 @@ public class RatMapActivity extends FragmentActivity implements OnMapReadyCallba
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    /**
+     * Displays the DatePickerFragment
+     * @param v the current view
+     */
+    public void showDatePickerDialog(View v) {
+        FragmentManager fm = getSupportFragmentManager();
+        DialogFragment newFragment = new AddReport.DatePickerFragment();
+        newFragment.show(fm, "datePicker");
+    }
+
+    /**
+     * Method that sets the click listeners for the buttons of the activity
+     */
+    private void setClickListeners() {
+        startDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(v);
+            }
+        });
+
+        endDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(v);
+            }
+        });
+
     }
 }
