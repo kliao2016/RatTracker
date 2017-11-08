9c5826e67ff04d9a778acac43c8ddd3a92e2aca3
ClosestExaminer <kyle71698@gmail.com>
2017-11-08 03:37:27 -0500

    fixed most GraphActivity warnings

--- RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/GraphActivity.java
+++ RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/GraphActivity.java
@@ -7,21 +7,22 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.icu.text.SimpleDateFormat;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -42,13 +43,16 @@ import java.util.Map;
 import edu.gatech.cs2340.rattracker.R;
 import edu.gatech.cs2340.rattracker.model.RatReport;
 
+/**
+ * creates a graph to display rat sightings on a monthly interval between two time spans
+ */
 public class GraphActivity extends AppCompatActivity {
 
     private static EditText graphStartDate;
     private static EditText graphEndDate;
     private Button selectRangeButton;
     private static Map<String, RatReport> reportMap = new HashMap<>();
-    private static Map<String, Integer> graphData = new HashMap<>();
+    private static final Map<String, Integer> graphData = new HashMap<>();
     private LineChart chart;
     private List<Entry> entries;
 
@@ -62,7 +66,7 @@ public class GraphActivity extends AppCompatActivity {
         chart.setDrawBorders(true);
         chart.setBorderWidth(4);
 
-        //Element set gives everything, reportMap.entryset has getKey getValue, Map.Entry
+        //Element set gives everything, reportMap entry set has getKey getValue, Map.Entry
         graphStartDate = findViewById(R.id.graph_start_date);
         graphEndDate = findViewById(R.id.graph_end_date);
         selectRangeButton = findViewById(R.id.graph_date_button);
@@ -74,96 +78,136 @@ public class GraphActivity extends AppCompatActivity {
         setClickListeners();
     }
 
+    private String[] parseGraphDates() {
+        String[] importantDates;
+        Editable startText = graphStartDate.getText();
+        String startTextForm = startText.toString();
+        Editable endText = graphEndDate.getText();
+        String endTextForm = endText.toString();
+        String startMonthSubString = startTextForm.substring(0, startTextForm.indexOf("/")); //"MM"
+        String remainder = startTextForm.substring(
+                startTextForm.indexOf("/")+1, startTextForm.length());
+        String startYearSubString = remainder.substring(
+                remainder.indexOf("/")+1, remainder.length()); //"yyyy"
+        String endMonthSubString = endTextForm.substring(0, endTextForm.indexOf("/")); //"MM"
+        remainder = endTextForm.substring(endTextForm.indexOf("/")+1, endTextForm.length());
+        String endYearSubString = remainder.substring(
+                remainder.indexOf("/")+1, remainder.length()); //"yyyy"
+
+        importantDates = new String[]{startMonthSubString, startYearSubString, endMonthSubString,
+            endYearSubString};
+        return importantDates;
+    }
+
+    private void populateGraph(int startMonthValue, int startYearValue,
+                               int totalMonthDifference) {
+        final int monthsInAYear = 12;
+        int currentMonthValue = startMonthValue;
+        int currentYearValue = startYearValue;
+        for (int i = 0; i < (totalMonthDifference + 1); i++) {
+            if (currentMonthValue > monthsInAYear) {
+                currentMonthValue = currentMonthValue % monthsInAYear;
+                currentYearValue += 1;
+                Log.d("GRAPH Incrementing", Integer.toString(currentYearValue));
+            }
+            String currentMonthString = Integer.toString(currentMonthValue);
+            String currentYearString = Integer.toString(currentYearValue);
+            String currentDateString = currentMonthString + "/" + currentYearString; //MM/yyyy
+            int dataToAdd;
+            if (graphData.containsKey(currentDateString)) {
+                dataToAdd = graphData.get(currentDateString);
+            } else {
+                dataToAdd = 0;
+            }
+            final float yearAdjuster = 10000f;
+            float adjustedYear = ((float)currentYearValue) / yearAdjuster;
+            float xValue = currentMonthValue + adjustedYear;
+            Log.d("GRAPH FINAL X VALUE", Float.toString(xValue));
+            Log.d("GRAPH FINAL Y VALUE", Float.toString(dataToAdd));
+            entries.add(new Entry(xValue, (float)(dataToAdd)));
+            currentMonthValue += 1;
+        }
+    }
+
+    private void setChartValues() {
+        entries = new ArrayList<>();
+        String[] importantDates = parseGraphDates();
+        int startMonthValue = Integer.parseInt(importantDates[0]);
+        int startYearValue = Integer.parseInt(importantDates[1]);
+        int endMonthValue = Integer.parseInt(importantDates[2]);
+        int endYearValue = Integer.parseInt(importantDates[3]);
+        final int monthsInAYear = 12;
+        int totalMonthDifference = (((endYearValue - startYearValue) * monthsInAYear)
+                + (endMonthValue - startMonthValue));
+        populateGraph(startMonthValue, startYearValue, totalMonthDifference);
+    }
+
+    private void populateDataHashMap(Date start, Date end, SimpleDateFormat format) {
+        for (Map.Entry<String, RatReport> ratReportEntry: reportMap.entrySet()) {
+            if (ratReportEntry != null) {
+                RatReport currentReport = ratReportEntry.getValue();
+                String dateCreated = currentReport.getDateCreated();
+                String dateTrimmed = dateCreated.substring(0,
+                        dateCreated.indexOf(' ')); ///  MM/dd/yyyy
+                Date ratEntryDate = new Date();
+                try {
+                    ratEntryDate = format.parse(dateTrimmed);
+                } catch (ParseException e) {
+                    Log.d("Date Parse Exception", e.getMessage());
+                }
+                //if the current report's date is within the selected months/years
+                //the # of reports made during that corresponding month is incremented
+                //in the graphData HashMap
+                if ((ratEntryDate.compareTo(start) >= 0)
+                        && (ratEntryDate.compareTo(end) <= 0)) {
+                    String evenShorter = dateCreated.substring(0, 3) //"MM/yyyy"
+                            + dateCreated.substring(6, dateCreated.indexOf(' '));
+                    if (graphData.containsKey(evenShorter)) {
+                        int oldReportCount = graphData.get(evenShorter);
+                        int reportCount = (oldReportCount + 1);
+                        graphData.put(evenShorter, reportCount);
+                    } else {
+                        graphData.put(evenShorter, 1);
+                    }
+                }
+            }
+        }
+    }
+
     /**
      * Method to load rat sightings from database and put them into the graphData
      */
     private void loadSightings() {
 
         // Load reports from reportMap and use their data to manipulate graphData
-        if (!isEmpty(graphStartDate) && !isEmpty(graphEndDate)) {
+        if (notEmpty(graphStartDate) && notEmpty(graphEndDate)) {
             SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
             Date startDate = new Date();
             Date endDate = new Date();
+
             try {
-                startDate = formatter.parse(graphStartDate.getText().toString().trim());
-                endDate = formatter.parse(graphEndDate.getText().toString().trim());
+
+                Editable startText = graphStartDate.getText();
+                String startString = startText.toString();
+                String startTrimmed = startString.trim();
+                startDate = formatter.parse(startTrimmed);
+
+                Editable endText = graphEndDate.getText();
+                String endString = endText.toString();
+                String endTrimmed = endString.trim();
+                endDate = formatter.parse(endTrimmed);
+
             } catch (ParseException e) {
                 Log.d("Date Parse Exception", e.getMessage());
             }
             if (startDate.compareTo(endDate) > 0) {
-                generateDateRangeAlert(R.string.valid_range_title,
-                        R.string.valid_range_info);
+                generateDateRangeAlert(
+                );
                 graphStartDate.setText("");
                 graphEndDate.setText("");
             } else {
-                for (Map.Entry<String, RatReport> ratReportEntry: reportMap.entrySet()) {
-                    if (ratReportEntry != null) {
-                        String dateCreated = ratReportEntry.getValue().getDateCreated();
-                        String dateTrimmed = dateCreated.substring(0,
-                                dateCreated.indexOf(' ')); ///  MM/dd/yyyy
-                        Date ratEntryDate = new Date();
-                        try {
-                            ratEntryDate = formatter.parse(dateTrimmed);
-                        } catch (ParseException e) {
-                            Log.d("Date Parse Exception", e.getMessage());
-                        }
-                        //if the current report's date is within the selected months/years
-                        //the # of reports made during that corresponding month is incremented
-                        //in the graphData HashMap
-                        if (ratEntryDate.compareTo(startDate) >= 0
-                                && ratEntryDate.compareTo(endDate) <= 0) {
-                            String evenShorter = dateCreated.substring(0, 3) //"MM/yyyy"
-                                    + dateCreated.substring(6, dateCreated.indexOf(' '));
-                            if (graphData.containsKey(evenShorter)) {
-                                int oldReportCount = graphData.get(evenShorter);
-                                int reportCount = (oldReportCount += 1);
-                                graphData.put(evenShorter, reportCount);
-                            } else {
-                                graphData.put(evenShorter, 1);
-                            }
-                        }
-                    }
-                }
-                ///LOGIC FOR USING GRAPHDATA HASHMAP TO MANIPULATE VALUES OF LINECHART
-                entries = new ArrayList<>();
-                String startTextForm = graphStartDate.getText().toString();
-                String endTextForm = graphEndDate.getText().toString();
-                String startMonthSubString = startTextForm.substring(0, startTextForm.indexOf("/")); //"MM"
-                int startMonthValue = Integer.parseInt(startMonthSubString);
-                String remainder = startTextForm.substring(startTextForm.indexOf("/")+1, startTextForm.length());
-                String startYearSubString = remainder.substring(remainder.indexOf("/")+1, remainder.length()); //"yyyy"
-                int startYearValue = Integer.parseInt(startYearSubString);
-                String endMonthSubString = endTextForm.substring(0, endTextForm.indexOf("/")); //"MM"
-                int endMonthValue = Integer.parseInt(endMonthSubString);
-                remainder = endTextForm.substring(endTextForm.indexOf("/")+1, endTextForm.length());
-                String endYearSubString = remainder.substring(remainder.indexOf("/")+1, remainder.length()); //"yyyy"
-                int endYearValue = Integer.parseInt(endYearSubString);
-                int totalMonthDifference = (((endYearValue - startYearValue) * 12)
-                        + (endMonthValue - startMonthValue));
-                int currentMonthValue = startMonthValue;
-                int currentYearValue = startYearValue;
-                for (int i = 0; i < totalMonthDifference + 1; i++) {
-                    if (currentMonthValue > 12) {
-                        currentMonthValue = currentMonthValue % 12;
-                        currentYearValue += 1;
-                        Log.d("GRAPH Incrementing", Integer.toString(currentYearValue));
-                    }
-                    String currentMonthString = Integer.toString(currentMonthValue);
-                    String currentYearString = Integer.toString(currentYearValue);
-                    String currentDateString = currentMonthString + "/" + currentYearString; //MM/yyyy
-                    int dataToAdd;
-                    if (graphData.containsKey(currentDateString)) {
-                        dataToAdd = graphData.get(currentDateString);
-                    } else {
-                        dataToAdd = 0;
-                    }
-                    float adjustedYear = ((float)currentYearValue) / 10000f;
-                    float xValue = currentMonthValue + adjustedYear;
-                    Log.d("GRAPH FINAL X VALUE", Float.toString(xValue));
-                    Log.d("GRAPH FINAL Y VALUE", Float.toString(dataToAdd));
-                    entries.add(new Entry(xValue, (float)(dataToAdd)));
-                    currentMonthValue += 1;
-                }
+                populateDataHashMap(startDate, endDate, formatter);
+                setChartValues();
             }
             LineDataSet dataSet = new LineDataSet(entries, "Sightings");
             LineData lineData = new LineData(dataSet);
@@ -205,11 +249,11 @@ public class GraphActivity extends AppCompatActivity {
      * Displays the DatePickerFragment
      * @param v the current view
      */
-    public void showDatePickerDialog(View v) {
+    private void showDatePickerDialog(View v) {
         FragmentManager fm = getSupportFragmentManager();
         myDatePickerFragment newFragment = new myDatePickerFragment();
         if (v.getId() == R.id.graph_start_date) {
-            newFragment.setStart(true);
+            newFragment.setStart();
         }
         newFragment.show(fm, "datePicker");
     }
@@ -217,14 +261,12 @@ public class GraphActivity extends AppCompatActivity {
     /**
      * Method to create custom alert dialog popup
      *
-     * @param title the title of the alert
-     * @param message the message of the alert
      */
-    private void generateDateRangeAlert(int title, int message) {
+    private void generateDateRangeAlert() {
         AlertDialog.Builder loginAlertBuilder = new AlertDialog.Builder(this);
-        loginAlertBuilder.setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(R.string.popup_button_okay,
+        loginAlertBuilder.setTitle(R.string.valid_range_title);
+        loginAlertBuilder.setMessage(R.string.valid_range_info);
+        loginAlertBuilder.setPositiveButton(R.string.popup_button_okay,
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int id) {
@@ -241,16 +283,21 @@ public class GraphActivity extends AppCompatActivity {
      * @param editText the EditText field to check
      * @return true if the EditText field is empty and false otherwise
      */
-    private boolean isEmpty(EditText editText) {
-        return editText.getText().toString().trim().length() == 0;
+    private boolean notEmpty(EditText editText) {
+        Editable theText = editText.getText();
+        String theString = theText.toString();
+        String theTrim = theString.trim();
+        return !theTrim.isEmpty();
     }
 
     /**
      * Defines a fragment that is shown when choosing the date that displays a calendar to the user
      */
-    public static class myDatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    public static class myDatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
         private boolean isStart;
 
+        @NonNull
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the current date as the default date in the picker
@@ -264,22 +311,30 @@ public class GraphActivity extends AppCompatActivity {
                     android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                     this,
                     year, month, day);
-            datePickerDialog.getWindow().setBackgroundDrawable(
+            Window datePickerWindow = datePickerDialog.getWindow();
+            assert datePickerWindow != null;
+            datePickerWindow.setBackgroundDrawable(
                     new ColorDrawable(Color.TRANSPARENT));
 
             // Create a new instance of DatePickerDialog and return it
             return datePickerDialog;
         }
 
-        public void setStart(boolean start) {
-            this.isStart = start;
+        /**
+         * determines which of the two EditText is affected
+         */
+        public void setStart() {
+            this.isStart = true;
         }
 
+        @Override
         public void onDateSet(DatePicker view, int year, int month, int day) {
             if (isStart) {
-                graphStartDate.setText((month + 1) + "/" + day + "/" + year);
+                String theString = (month + 1) + "/" + day + "/" + year;
+                graphStartDate.setText(theString);
             } else {
-                graphEndDate.setText((month + 1) + "/" + day + "/" + year);
+                String theString = (month + 1) + "/" + day + "/" + year;
+                graphEndDate.setText(theString);
             }
         }
     }
@@ -289,16 +344,17 @@ public class GraphActivity extends AppCompatActivity {
      */
     private class LoadSightingsTask extends AsyncTask<Void, Void, Map<String, RatReport>> {
         private ProgressBar progress;
-        private Map<String, RatReport> asyncMap = new HashMap<>();
+        private final Map<String, RatReport> asyncMap = new HashMap<>();
 
-        private Query databaseRef = FirebaseDatabase.getInstance()
+        final int instanceCap = 300;
+        private final Query databaseRef = FirebaseDatabase.getInstance()
                 .getReference()
                 .child("reports")
-                .limitToLast(300);
+                .limitToLast(instanceCap);
 
         @Override
         protected void onPreExecute() {
-            progress = (ProgressBar) findViewById(R.id.graph_progress_bar);
+            progress = findViewById(R.id.graph_progress_bar);
             progress.setVisibility(View.VISIBLE);
         }
 
@@ -325,7 +381,7 @@ public class GraphActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Map<String, RatReport> ratReports) {
-            reportMap = ratReports;
+            reportMap = asyncMap;
             progress.setVisibility(View.GONE);
         }
     }
