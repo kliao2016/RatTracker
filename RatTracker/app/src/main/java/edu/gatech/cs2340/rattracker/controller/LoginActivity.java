69429a72f83c1ccdf83ab710fb25727937ce056e
Kevin Liao <kliao2016@gmail.com>
2017-11-07 22:56:13 -0500

    Fixed LoginActivity code style errors

--- RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/LoginActivity.java
+++ RatTracker/app/src/main/java/edu/gatech/cs2340/rattracker/controller/LoginActivity.java
@@ -2,6 +2,7 @@ package edu.gatech.cs2340.rattracker.controller;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,17 +14,18 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import edu.gatech.cs2340.rattracker.R;
-import edu.gatech.cs2340.rattracker.model.User;
 
+/**
+ * Class that displays UI to handle user login
+ *
+ * Created by Kevin Liao on 9/15/17
+ */
 public class LoginActivity extends AppCompatActivity {
 
-    private Button loginButton;
-    private Button cancelLoginButton;
     private EditText emailEditText;
     private EditText passEditText;
     private FirebaseAuth auth;
-    private static boolean loginSuccess = false;
-    public User user;
+    private boolean loginSuccess;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,16 +33,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         // Initialize fields
-        loginButton = ((Button) findViewById(R.id.loginButton));
-        cancelLoginButton = ((Button) findViewById(R.id.cancelLoginButton));
-        emailEditText = ((EditText) findViewById(R.id.emailEditText));
-        passEditText = ((EditText) findViewById(R.id.passEditText));
+        Button loginButton = findViewById(R.id.loginButton);
+        Button cancelLoginButton = findViewById(R.id.cancelLoginButton);
+        emailEditText = findViewById(R.id.emailEditText);
+        passEditText = findViewById(R.id.passEditText);
 
         // Set button actions
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!isEmpty(emailEditText) && !isEmpty(passEditText)) {
+                if (isFilled(emailEditText) && isFilled(passEditText)) {
                     signInUser(emailEditText.getText().toString().trim(),
                                passEditText.getText().toString().trim());
                 } else {
@@ -60,7 +62,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
-        // Initialize Firebase Auth
+        // Initialize FireBase Auth
         auth = FirebaseAuth.getInstance();
     }
 
@@ -74,13 +76,12 @@ public class LoginActivity extends AppCompatActivity {
         this.auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onComplete(Task<AuthResult> task) {
+                    public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             loginSuccess = true;
                             generateLoginAlert(R.string.login_success_title,
                                                R.string.login_success_message);
-                            user = new User("Username", password, email);
                         } else {
                             // If sign in fails, display a message to the user.
                             loginSuccess = false;
@@ -121,8 +122,8 @@ public class LoginActivity extends AppCompatActivity {
      * @param editText the EditText field to check
      * @return true if the EditText field is empty and false otherwise
      */
-    private boolean isEmpty(EditText editText) {
-        return editText.getText().toString().trim().length() == 0;
+    private boolean isFilled(EditText editText) {
+        return editText.getText().toString().trim().length() != 0;
     }
 
     /**
